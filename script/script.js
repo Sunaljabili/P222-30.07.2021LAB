00326ff8a85819d87d4333ca7caa885ef04223b9
Sunaljabili <you@example.com>
2021-07-30 17:09:34 +0400

    Lab Tasks

---
+++ script/script.js
@@ -0,0 +1,49 @@
+// Task1
+// let a=7;
+// let b=3;
+
+// a=a+b; //10
+// b=a-b; //10-3
+// a=a-b; //10-7
+
+// console.log(`a nin deyeri ${a} b-nin deyeri${b}`)
+
+// Task2
+
+// let n=1; 
+// let m=7;
+// let sum=0;
+// for (;n<=m;n++) {
+//     sum +=n;
+// }
+// console.log(sum)
+
+//Task 3
+
+// let x=10;
+// let y=12;
+// let z=14;
+
+// let a=20;
+// let b=15;
+
+// if((x<a&&y<b) || (x<b&&y<a)|| (z<a&& x<b) ||(z<b && x<a) || (z<a&& y<b) || (z<b&&y<a)){
+// console.log("şkaf qapıdan kecir")
+// }else{
+//     console.log("şkaf qapıdan kecmir")
+// }
+
+//Task 4
+
+// let n= 15;
+// let newNumber=1;
+
+//  for(newNumber=1;newNumber<n;){
+//      newNumber*=2
+//  }
+//  if(newNumber==n){
+//      console.log("2 nin quvvetidir")
+//  }else{
+//      console.log("2 nin quvveti deil;")
+//  }
+
